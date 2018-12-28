741fbab1d8ca5263abd783de3e507b80dfcd0743
Yizack Rangel <yizackr@gmail.com>
2018-12-28 03:07:44 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+# Yeezac
+Bot de música para Discord con Node.js
+
+## Requerimientos Generales
+- Crear [Discord Bot](https://discordapp.com/developers/applications/)
+- Habilitar [Youtube API](https://console.developers.google.com/)
+
+### Configuración General
+- Invita el bot a tu servidor de Discord accediendo en\
+`https://discordapp.com/oauth2/authorize?client_id=XXXXXXXXXXXXXXXXXX&scope=bot&permissions=301296759`. \
+Reemplaza `XXXXXXXXXXXXXXXXXX` por el **Client ID** del Discord Bot.
+- Editar `config.json`
+  - `discord_token` Reemplazar `bot token` con el token del Discord Bot.
+  - `botid` Reemplazar `id del bot` con el id del Discord Bot.
+  - `prefix` Reemplazar `y!` con el prefijo que desees para ejecutar un comando en Discord.
+  - `yt_api_key` Reemplazar `api key de youtube` con la api key de Youtube.
+
+## Requerimientos para instalar en una PC local (Windows)
+- Descargar e Instalar [Node.js](https://nodejs.org/)
+
+### Configurar
+- Abrir `cmd.exe` en Windows.
+- Ejecutar `cd\` para ir a la raíz del disco local.
+- Ejecutar para ir al directorio donde están los archivos del Discord Bot. Ejemplo `cd %USERPROFILE%\Documents\DiscordBot`
+- Ejecutar `npm install ffmpeg --save` para instalar la librería ffmpeg.
+- Ejecutar `node index.js` para activar el Discord Bot.
+
+## Requerimientos para instalar en Heroku (Windows)
+- Registro en [Heroku](https://heroku.com/)
+- Crear [Heroku App](https://dashboard.heroku.com/new-app)
+- Descargar e Instalar [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli)
+- Descargar e Instalar [Git](https://git-scm.com/downloads)
+
+### Configurar
+- Asignar nombre a la app en Heroku.
+- Ir a la pestaña **Settings** de tu Heroku App, en el botón **Add Buildpack**:
+  - Agrega un buildpack, selecciona la opcion `nodejs` y presiona **Save Changes** para instalar Node.js en Heroku.
+  - Agrega otro buildpack, ingresa el siguiente URL `https://github.com/issueapp/heroku-buildpack-ffmpeg` y presiona **Save Changes** para instalar la librería ffmpeg compatible con Heroku.
+- Abrir `cmd.exe` en Windows.
+- Ejecutar `heroku login` y presiona cualquier tecla que no sea `q` para iniciar sesión.
+- Ejecutar `cd\` para ir a la raíz del disco local.
+- Ejecutar para ir al directorio donde están los archivos del Discord Bot. Ejemplo `cd %USERPROFILE%\Documents\DiscordBot`
+- Ejecutar `git init` para inicializar los archivos **.git**
+- Ejecutar `heroku git:remote -a nombredelaapp` para controlar tu Heroku App. Reemplaza `nombredelaapp` por el nombre que le asignaste a tu Heroku App.
+- Ejecutar `git add .` para añadir los archivos del Discord Bot a Heroku.
+- Ejecutar `git commit -am "make it better"` para guardar los cambios
+- Ejecutar `git push heroku master` subir los archivos a Heroku.
+- Ejecutar `heroku ps:scale worker=1` para activar el Discord Bot 24/7.
+
+## Uso
+| Comando | Descripción
+|---------|-------------|
+| `y!play` | Reproduce una canción de Youtube. |
+| `y!cola` | Muestra las canciones que están en cola de reproducción. |
+| `y!skip` | Salta la canción que se está reproduciendo. |
+| `y!comandos` | Muestra la lista de comandos. |
+| `y!salir` | El Discord Bot sale de el canal de voz. |
+
+## Sugerencias, preguntas o problemas
+Si tienes sugerencias, preguntas o algún problema, no dudes en escribeme en [@Yizack](https://github.com/Yizack/yeezac/issues/new).
---
+++ index.js
@@ -0,0 +1,224 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ytdl = require("ytdl-core");
+const request = require("request");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
+const config = require("./config.json");
+
+const yt_api_key = config.yt_api_key;
+const prefix = config.prefix;
+const discord_token = config.discord_token;
+const botid = config.botid;
+
+var guilds = {};
+
+client.login(discord_token);
+
+client.on('message', function(message) {
+    const member = message.member;
+    const mess = message.content.toLowerCase();
+    const args = message.content.split(' ').slice(1).join(" ");
+
+    if (!guilds[message.guild.id]) {
+        guilds[message.guild.id] = {
+            queue: [],
+            queueNames: [],
+            isPlaying: false,
+            dispatcher: null,
+            voiceChannel: null,
+            skipReq: 0,
+            skippers: [],
+        };
+    }
+
+    if (mess.startsWith(prefix + "play")) {
+        if (message.member.voiceChannel) {
+			if(mess === (prefix + "play")){
+				message.reply("No escribiste el nombre de ninguna canción.");
+			}		
+			else{
+				if (guilds[message.guild.id].queue.length > 0 || guilds[message.guild.id].isPlaying) {
+					getID(args, function(id) {
+						add_to_queue(id, message);
+						fetchVideoInfo(id, function(err, videoInfo) {
+							if (err) throw new Error(err);
+							message.reply("📢 Has añadido una canción a la cola: ```🎵: " + videoInfo.title + "\n⏲️: [" + duracion(videoInfo.duration) +  "]\n📽️: " + videoInfo.url + "```");
+							guilds[message.guild.id].queueNames.push(videoInfo.title + ", ⏲️: [" + duracion(videoInfo.duration) + "]");
+						});
+					});
+				} else {
+					isPlaying = true;
+					getID(args, function(id) {
+						guilds[message.guild.id].queue.push(id);
+						fetchVideoInfo(id, function(err, videoInfo) {
+							if (err) throw new Error(err);
+							guilds[message.guild.id].queueNames.push(videoInfo.title + ", ⏱️: [" + duracion(videoInfo.duration) + "]");
+						});
+						playMusic(id, message);
+					});
+				}
+			}
+        } else {
+            message.reply(" Necesitas unirte a un canal de voz!");
+        }
+    } else if (mess.startsWith(prefix + "skip")) {
+            guilds[message.guild.id].skippers.push(message.author.id);
+            guilds[message.guild.id].skipReq++;
+            skip_song(message);
+            message.reply(" La canción ha sido saltada!");
+    } else if (mess.startsWith(prefix + "cola")) {
+        var message2 = "```css\n";
+        for (var i = 0; i < guilds[message.guild.id].queueNames.length; i++) {
+            var temp = (i + 1) + ": " + (i === 0 ? "🔊 " : "") + guilds[message.guild.id].queueNames[i] + "\n";
+            if ((message2 + temp).length <= 2000 - 3) {
+                message2 += temp;
+            } else {
+                message2 += "```";
+                message.channel.send(message2);
+                message2 = "```";
+            }
+        }
+        message2 += "```";
+        message.channel.send(message2);
+    }
+	else if (mess.startsWith(prefix + "reparar")) {
+		if(guilds[message.guild.id].voiceChannel != null){
+			guilds[message.guild.id].voiceChannel.leave();
+			sleep(100);
+			guilds[message.guild.id].voiceChannel.join();
+			client.user.setPresence({
+				game: {
+				name: "",
+				type: 0
+				}
+			});
+		}
+	}
+	else if (mess.startsWith(prefix + "salir")) {
+		if(guilds[message.guild.id].voiceChannel != null){
+			guilds[message.guild.id].voiceChannel.join();
+			sleep(100);
+			guilds[message.guild.id].voiceChannel.leave();
+			guilds[message.guild.id].queue = [];
+			client.user.setPresence({
+				game: {
+				name: "",
+				type: 0
+				}
+			});
+		}
+	}
+	else if (mess.startsWith(prefix + "comandos")) {
+		message.channel.send(	"📜 Lista de comandos:\n"+
+								"```xl\n"+
+								"'y!play' Reproducir una canción o añadirla a la cola.\n"+
+								"'y!cola' Ver lista de canciones en cola.\n"+
+								"'y!skip' Saltar la canción que se está reproduciendo.\n"+
+								"'y!reparar' Reparar el bot por si no quiere reproducir las canciones (Se eliminarán todas las canciones en la cola).\n"+
+								"'y!salir' Sacar el bot del chat de voz por si no se sale automáticamente."+"```");
+    }
+});
+
+client.on('ready', function() {
+    console.log("Estoy listo!");
+});
+
+function skip_song(message) {
+    guilds[message.guild.id].dispatcher.end();
+}
+
+function playMusic(id, message) {
+	fetchVideoInfo(id, function(err, videoInfo) {
+        if (err) throw new Error(err);
+		console.log("ID: "+id);
+		message.channel.send("🔊 Se está reproduciendo:```fix\n🎵: " + videoInfo.title + "\n⏲️: [" + duracion(videoInfo.duration) +  "]\n📽️: " + videoInfo.url + "```");
+		console.log(message.author.username + " está reproduciendo: " + videoInfo.title);
+		client.user.setPresence({
+			game: {
+            name: videoInfo.title,
+            type: 0
+			}
+		});
+    });
+    guilds[message.guild.id].voiceChannel = message.member.voiceChannel;
+    guilds[message.guild.id].voiceChannel.join().then(connection => {
+        const stream = ytdl("https://www.youtube.com/watch?v=" + id, {
+            filter: "audioonly"
+        });
+        guilds[message.guild.id].skipReq = 0;
+        guilds[message.guild.id].skippers = [];
+        guilds[message.guild.id].dispatcher = connection.playStream(stream);
+        guilds[message.guild.id].dispatcher.on('end', function() {
+            guilds[message.guild.id].skipReq = 0;
+            guilds[message.guild.id].skippers = [];
+            guilds[message.guild.id].queue.shift();
+            guilds[message.guild.id].queueNames.shift();
+            if (guilds[message.guild.id].queue.length === 0) {
+                guilds[message.guild.id].queue = [];
+                guilds[message.guild.id].queueNames = [];
+                guilds[message.guild.id].isPlaying = false;
+				guilds[message.guild.id].voiceChannel.leave()
+				client.user.setPresence({
+					game: {
+                    name: "",
+                    type: 0
+					}
+				});
+            } else {
+                setTimeout(function() {
+                    playMusic(guilds[message.guild.id].queue[0], message);
+                }, 500);
+            }
+        });
+    }).catch(err => console.log(err));;
+}
+
+function getID(str, cb) {
+    if (isYoutube(str)) {
+        cb(getYouTubeID(str));
+    } else {
+        search_video(str, function(id) {
+            cb(id);
+        });
+    }
+}
+
+function add_to_queue(strID, message) {
+    if (isYoutube(strID)) {
+        guilds[message.guild.id].queue.push(getYouTubeID(strID));
+    } else {
+        guilds[message.guild.id].queue.push(strID);
+    }
+}
+
+function search_video(query, callback) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+        var json = JSON.parse(body);
+        if (!json.items[0]) callback("3_-a9nVZYjk");
+        else {
+            callback(json.items[0].id.videoId);
+        }
+    });
+}
+
+function isYoutube(str) {
+    return str.toLowerCase().indexOf("youtube.com") > -1;
+}
+
+function duracion(time){   
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60;
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
